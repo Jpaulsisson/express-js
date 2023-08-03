b69f041c777bfa1bdde6b533b5198c10a785f050
Paul Sisson <paulsissonsemail@gmail.com>
2023-08-03 16:04:13 -0500

    Add notes and examples of middleware functions

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ userRouter.get('/new', (req, res) => {
 
 userRouter
 .route('/:id')
-.get((req, res) => {
+.get(logIt, (req, res) => {
   res.send(`Get User Page where the ID is ${req.params.id}`)
 })
 .put((req, res) => {
@@ -29,5 +29,10 @@ userRouter.param('id', (req, res, next, id) => {
   console.log(id);
   next();
 })
+
+function logIt(req, res, next) {
+  console.log(req.originalUrl)
+  next();
+}
 
 module.exports = userRouter;
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
 
+//note: declaring a function with a const variable makes it inaccessable to things before it's initialization * see other note near bottom for more
+const authorize = (req, res, next) => {
+  console.log(`${req.originalUrl} is AUTHORIZED.`);     
+  next();
+}
+
+app.use(authorize, betterWay)
 app.set('view engine', 'ejs');
 
-
-
-
-app.get('/', (req, res) => {
+app.get('/', authorize, (req, res) => {
   res.render('index', {doesNotMatter: 'pardner'})
 })
 
-
 const userRouter = require('./routes/users')
-
 app.use('/users', userRouter);
 
+//other note: declaring a function in this way makes it accessable to all things above and below it
+function betterWay(req, res, next) {
+  console.log('This is the way')
+  next();
+}
 
 app.listen(3000);
